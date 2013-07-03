13949c1dbb38ce19ab0aa37783a9642548377c8a
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-07-03 10:42:05 +0900

    zoom added

--- R/d3ForceNetwork.R
+++ R/d3ForceNetwork.R
@@ -14,6 +14,7 @@
 #' @param charge numeric value indicating either the strength of the node repulsion (negative value) or attraction (positive value).
 #' @param linkColour character string specifying the colour you want the link lines to be. Multiple formats supported (e.g. hexadecimal).
 #' @param opacity numeric value of the proportion opaque you would like the graph elements to be.
+#' @param zoom logical, whether or not to enable the ability to use the mouse scroll-wheel to zoom in and out of the graph.
 #' @param standAlone logical, whether or not to return a complete HTML document (with head and foot) or just the script.
 #' @param file a character string of the file name to save the resulting graph. If a file name is given a standalone webpage is created, i.e. with a header and footer. If \code{file = NULL} then result is returned to the console. 
 #' @param iframe logical. If \code{iframe = TRUE} then the graph is saved to an external file in the working directory and an HTML \code{iframe} linking to the file is printed to the console. This is useful if you are using Slidify and many other HTML slideshow framworks and want to include the graph in the resulting page. If you set the knitr code chunk \code{results='asis'} then the graph will be rendered in the output. Usually, you can use \code{iframe = FALSE} if you are creating simple knitr Markdown or HTML pages. Note: you do not need to specify the file name if \code{iframe = TRUE}, however if you do, do not include the file path.
@@ -40,7 +41,7 @@
 #'
 #' @export
 
-d3ForceNetwork <- function(Links, Nodes, Source, Target, Value = NULL, NodeID, Group, height = 600, width = 900, fontsize = 7, linkDistance = 50, charge = -120, linkColour = "#666", opacity = 0.6, standAlone = TRUE, file = NULL, iframe = FALSE, d3Script = "http://d3js.org/d3.v3.min.js") 
+d3ForceNetwork <- function(Links, Nodes, Source, Target, Value = NULL, NodeID, Group, height = 600, width = 900, fontsize = 7, linkDistance = 50, charge = -120, linkColour = "#666", opacity = 0.6, zoom = FALSE, standAlone = TRUE, file = NULL, iframe = FALSE, d3Script = "http://d3js.org/d3.v3.min.js") 
 {
 if (!isTRUE(standAlone) & isTRUE(iframe)){
     stop("If iframe = TRUE then standAlone must be TRUE.")
@@ -90,7 +91,12 @@ if (!isTRUE(standAlone) & isTRUE(iframe)){
 	NetworkCSS <- whisker.render(ForceMainStyleSheet())
 
 	# Main script for creating the graph
-	MainScript <- whisker.render(MainForceJS())
+	if (!isTRUE(zoom)){
+		MainScript <- whisker.render(MainForceJS())
+	} 
+	else if (isTRUE(zoom)){
+		MainScript <- whisker.render(ForceZoomJS())
+	} 
 
 	if (is.null(file) & !isTRUE(standAlone)){
 		cat(NetworkCSS, LinkData, NodesData, MainScript)
--- R/templates.R
+++ R/templates.R
@@ -262,3 +262,99 @@ d3.select(this).select(\"circle\").transition()
 
 </script>\n"
 }
+
+#' Mustache main Force Directed Network template for d3ForceNetwork with zooming capabilities
+#' 
+#' @keywords internals
+#' @noRd
+
+ForceZoomJS <- function(){
+"var width = {{width}}
+height = {{height}};
+
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+.nodes(d3.values(nodes)) 
+.links(links) 
+.size([width, height]) 
+.linkDistance({{linkDistance}}) 
+.charge({{charge}}) 
+.on(\"tick\", tick) 
+.start(); 
+
+var svg = d3.select(\"body\").append(\"svg\")
+.attr(\"width\", width)
+.attr(\"height\", height)
+.attr(\"pointer-events\", \"all\")
+.call(d3.behavior.zoom().on(\"zoom\", redraw));
+
+var vis = svg
+.append(\"svg:g\");
+
+vis.append(\"svg:rect\")
+.attr(\"width\", width)
+.attr(\"height\", height)
+.attr(\"fill\", 'white');
+
+function redraw() {
+vis.attr(\"transform\",
+\"translate(\" + d3.event.translate + \")\"
++ \" scale(\" + d3.event.scale + \")\");
+}
+
+var link = vis.selectAll(\".link\")
+.data(force.links())
+.enter().append(\"line\")
+.attr(\"class\", \"link\")
+.style(\"stroke-width\", function(d) { return Math.sqrt(d.value); });
+
+var node = vis.selectAll(\".node\")
+.data(force.nodes())
+.enter().append(\"g\") 
+.attr(\"class\", \"node\")
+.style(\"fill\", function(d) { return color(d.group); })
+.style(\"opacity\", {{opacity}})
+.on(\"mouseover\", mouseover) 
+.on(\"mouseout\", mouseout) 
+.call(force.drag);
+
+node.append(\"circle\") 
+.attr(\"r\", 6)
+
+node.append(\"svg:text\")
+.attr(\"class\", \"nodetext\")
+.attr(\"dx\", 12)
+.attr(\"dy\", \".35em\")
+.text(function(d) { return d.name });
+
+function tick() { 
+link 
+.attr(\"x1\", function(d) { return d.source.x; }) 
+.attr(\"y1\", function(d) { return d.source.y; }) 
+.attr(\"x2\", function(d) { return d.target.x; }) 
+.attr(\"y2\", function(d) { return d.target.y; }); 
+
+node.attr(\"transform\", function(d) { return \"translate(\" + d.x + \",\" + d.y + \")\"; }); 
+} 
+
+function mouseover() { 
+d3.select(this).select(\"circle\").transition() 
+.duration(750) 
+.attr(\"r\", 16);
+d3.select(this).select(\"text\").transition()
+.duration(750)
+.attr(\"x\", 13)
+.style(\"stroke-width\", \".5px\")
+.style(\"font\", \"{{clickTextSize}}px serif\")
+.style(\"opacity\", 1); 
+} 
+
+function mouseout() { 
+d3.select(this).select(\"circle\").transition() 
+.duration(750) 
+.attr(\"r\", 8); 
+} 
+
+</script>\n"
+}
